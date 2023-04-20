c85b7b98e6e1b156407ba072551633393fb9b230
Kevin Feliz Henriquez <108124473+programmerKFH@users.noreply.github.com>
2023-04-20 03:14:45 -1100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const URL = 'https://api.thecatapi.com/v1/images/search'
-
-fetch(URL)
-    .then(rest => rest.json())
-    .then(data => {
-        const img = document.querySelector('img')
-        img.src = data[0].url
+document.addEventListener('DOMContentLoaded', function (){
+    catAPI()
+    const btnCat = document.querySelector('#cat-button')
+    btnCat.addEventListener('click', async function(){
+        await catAPI()
     })
+})
+
+async function catAPI() {
+    const URL = 'https://api.thecatapi.com/v1/images/search';
+  
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      const img = document.querySelector('img');
+      img.src = data[0].url;
+    } catch (error) {
+      console.error(error);
+    }
+}
